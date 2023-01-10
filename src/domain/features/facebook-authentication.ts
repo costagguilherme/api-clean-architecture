799263dc05e0dd503b4798a163aba278a0a1ba51
Guilherme Lopes <mobills.guilherme.lo@toro.int.br>
2023-01-10 20:04:47 -0300

    feat: add facebook authentication

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,15 @@
+import { AuthenticationError } from './../errors/authentication';
+import { AccessToken } from './../models/access-token';
+
+export interface IFacebookAuthentication {
+    perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+namespace FacebookAuthentication {
+    export type Params = {
+        token: string
+    }
+
+    export type Result = AccessToken | AuthenticationError
+    
+}
