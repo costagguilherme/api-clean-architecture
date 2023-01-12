e5404f024909185311e63982e7910c41ffae9b0f
Guilherme Lopes <mobills.guilherme.lo@toro.int.br>
2023-01-12 19:41:47 -0300

    ajustando nome da classe

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,23 +3,26 @@ import { FacebookAuthenticationService } from './../../../src/data/services';
 import { ILoadFacebookUserApi, LoadFacebookUserApi } from './../../../src/data/contracts/apis';
 
 
-class LoadFacebookUserByTokenApiSpy implements ILoadFacebookUserApi {
-
+class LoadFacebookUserApiSpy implements ILoadFacebookUserApi {
     token?: string
     result = undefined
+    callsCount = 0
 
     async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
         this.token = params.token
+        this.callsCount++
         return this.result
     }
 }
 
 describe('FacebookAuthenticationService', () => {
     it('should call LoadFacebookUserApi with correct params', async () => {
-        const loadFacebookUserApi = new LoadFacebookUserByTokenApiSpy();
+        const loadFacebookUserApi = new LoadFacebookUserApiSpy();
         const sut = new FacebookAuthenticationService(loadFacebookUserApi)
         await sut.perform({token: 'any_token'})
         expect(loadFacebookUserApi.token).toBe('any_token')
+        expect(loadFacebookUserApi.callsCount).toBe(1)
+
     })
 
     it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
