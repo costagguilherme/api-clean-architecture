fcb329b2d92bbf3eb920b73b529a2f4dbd977e87
Guilherme Lopes <mobills.guilherme.lo@toro.int.br>
2023-01-12 20:04:12 -0300

    factory para devolver sut

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -2,22 +2,31 @@ import { AuthenticationError } from './../../../src/domain/errors/authentication
 import { FacebookAuthenticationService } from './../../../src/data/services';
 import { ILoadFacebookUserApi, LoadFacebookUserApi } from './../../../src/data/contracts/apis';
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+type SutTypes = {
+    sut: FacebookAuthenticationService
+    loadFacebookUserApi: MockProxy<ILoadFacebookUserApi>
+}
+
+function makeSut(): SutTypes {
+    const loadFacebookUserApi = mock<ILoadFacebookUserApi>()
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    return {sut, loadFacebookUserApi}
+}
 
 
 describe('FacebookAuthenticationService', () => {
     it('should call LoadFacebookUserApi with correct params', async () => {
-        const loadFacebookUserApi = mock<ILoadFacebookUserApi>()
-        const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+        const  { sut, loadFacebookUserApi } = makeSut()
         await sut.perform({token: 'any_token'})
         expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token'})
         expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
     })
 
     it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-        const loadFacebookUserApi = mock<ILoadFacebookUserApi>()
+        const  { sut, loadFacebookUserApi } = makeSut()
         loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
-        const sut = new FacebookAuthenticationService(loadFacebookUserApi)
         const authResult = await sut.perform({token: 'any_token'})
         console.log(authResult)
         expect(authResult).toEqual(new AuthenticationError());
